5a335d30040cee2f86c7abcbeb3d3d6f830ff6e0
ruairig <ruairigray@gmail.com>
2016-06-07 16:55:14 +1200

    login

--- login.php
+++ login.php
@@ -1,8 +1,17 @@
-<html>
-<head>
-    <title>PHP Test</title>
-</head>
-<body>
-<?php echo '<p>Hello World</p>'; ?>
-</body>
-</html>
+<div class="login-page">
+    <div class="form">
+        <form class="register-form">
+            <input type="text" placeholder="name"/>
+            <input type="password" placeholder="password"/>
+            <input type="text" placeholder="email address"/>
+            <button>create</button>
+            <p class="message">Already registered? <a href="#">Sign In</a></p>
+        </form>
+        <form class="login-form">
+            <input type="text" placeholder="username"/>
+            <input type="password" placeholder="password"/>
+            <button>login</button>
+            <p class="message">Not registered? <a href="#">Create an account</a></p>
+        </form>
+    </div>
+</div>
