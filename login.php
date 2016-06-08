e64536b1328de60b577a3f6199ce9979dc6802e0
ruairig <ruairigray@gmail.com>
2016-06-08 18:14:46 +1200

    index page

--- login.php
+++ login.php
@@ -55,16 +55,6 @@
     </div>
 </nav>
 
-<div class="container">
-
-    <div class="starter-template">
-        <h1>Bootstrap starter template</h1>
-        <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
-    </div>
-
-</div><!-- /.container -->
-
-
 <!-- Bootstrap core JavaScript
 ================================================== -->
 <!-- Placed at the end of the document so the pages load faster -->
